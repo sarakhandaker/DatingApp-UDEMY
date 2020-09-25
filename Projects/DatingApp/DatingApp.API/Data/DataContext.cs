5644cf074686b33d6a52b9ab5a574c3c4699affa
sarakhandaker <kttsara@gmail.com>
2020-09-25 13:17:19 -0700

    added user model

--- Projects/DatingApp/DatingApp.API/Data/DataContext.cs
+++ Projects/DatingApp/DatingApp.API/Data/DataContext.cs
@@ -9,5 +9,6 @@ namespace DatingApp.API.Data
         {}
 // name of property below is name of table in database
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
-}
+} 
