850286f023d80fdbf09943f729976fb03411db71
sarakhandaker <kttsara@gmail.com>
2020-10-05 14:46:22 -0700

    register form added

--- Projects/DatingApp/DatingApp-SPA/src/app/app.module.ts
+++ Projects/DatingApp/DatingApp-SPA/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [			
     AppComponent,
       ValueComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
   imports: [
     BrowserModule,
--- Projects/DatingApp/DatingApp-SPA/src/app/nav/nav.component.ts
+++ Projects/DatingApp/DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,13 +10,31 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService ) { }
 
+  // tslint:disable-next-line: typedef
   ngOnInit() {
   }
 
+  // tslint:disable-next-line: typedef
   login() {
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in succesfully');
+    }, error => {
+      console.log('Failed to login');
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  // tslint:disable-next-line: typedef
+  logOut(){
+    localStorage.removeItem('token');
+    console.log('logged out');
   }
 
 }
