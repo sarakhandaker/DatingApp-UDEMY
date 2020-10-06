090b4725acdb376e547528ff35e2819b21760d07
sarakhandaker <kttsara@gmail.com>
2020-10-06 14:21:15 -0700

    angular error handling

--- Projects/DatingApp/DatingApp-SPA/src/app/app.module.ts
+++ Projects/DatingApp/DatingApp-SPA/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { RegisterComponent } from './register/register.component';
     FormsModule
   ],
   providers: [
-    AuthService
+    AuthService,
+    ErrorInterceptorProvider
   ],
   bootstrap: [AppComponent]
 })
