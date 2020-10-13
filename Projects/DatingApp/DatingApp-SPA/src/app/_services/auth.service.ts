506ea72f508cf612c192b5b40a4a81676e3df73b
sarakhandaker <kttsara@gmail.com>
2020-10-13 11:41:23 -0700

    added user service

--- Projects/DatingApp/DatingApp-SPA/src/app/_services/auth.service.ts
+++ Projects/DatingApp/DatingApp-SPA/src/app/_services/auth.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import {map} from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-baseURL = 'http://localhost:5000/api/auth/';
+baseURL = environment.apiUrl + 'auth/';
 jwtHelper = new JwtHelperService();
 decodedToken: any;
 constructor(private http: HttpClient) { }
--- Projects/DatingApp/DatingApp-SPA/src/app/_services/user.service.ts
+++ Projects/DatingApp/DatingApp-SPA/src/app/_services/user.service.ts
@@ -1,5 +1,14 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { User } from '../_models/user';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Authorization': 'Bearer ' + localStorage.getItem('token')
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +17,14 @@ export class UserService {
 
 baseUrl = environment.apiUrl;
 
-constructor() { }
+constructor(private http: HttpClient) { }
+
+getUsers(): Observable<User[]>{
+  return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
+}
+
+getUser(id: number): Observable<User>{
+  return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
+}
 
 }
