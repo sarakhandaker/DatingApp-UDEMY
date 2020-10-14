2d581a6bbc44ab7d5f9a162cc61de2e382802708
sarakhandaker <kttsara@gmail.com>
2020-10-14 14:23:50 -0700

    http put method for edit user

--- Projects/DatingApp/DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ Projects/DatingApp/DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -20,6 +20,7 @@ namespace DatingApp.API.Helpers
                 .ForMember(des => des.Age, opt =>
                     opt.MapFrom(src=> src.DateOfBirth.CalculateAge() ));
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
