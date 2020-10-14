755d0f080b80abc5ad15615f98c3c76463d7db2c
sarakhandaker <kttsara@gmail.com>
2020-10-14 13:50:40 -0700

    form reset added

--- Projects/DatingApp/DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ Projects/DatingApp/DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/user';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -8,15 +10,21 @@ import { User } from 'src/app/_models/user';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm', {static: true}) editFrom: NgForm;
   user: User;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data.user;
     }
     );
+  }
+
+  updateUser() {
+    this.alertify.success('Profile updated successfully!');
+    this.editFrom.reset(this.user);
   }
 
 }
