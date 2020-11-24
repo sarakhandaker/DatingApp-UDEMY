feee3ec439c6a4556be2d773245437f922766fd8
sarakhandaker <kttsara@gmail.com>
2020-11-24 15:52:59 -0800

    Adding the Set Main Photo functionality to the SPA

--- Projects/DatingApp/DatingApp-SPA/src/app/_services/user.service.ts
+++ Projects/DatingApp/DatingApp-SPA/src/app/_services/user.service.ts
@@ -5,24 +5,28 @@ import { environment } from '../../environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  baseUrl = environment.apiUrl;
 
-baseUrl = environment.apiUrl;
+  constructor(private http: HttpClient) {}
 
-constructor(private http: HttpClient) { }
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl + 'users');
+  }
 
-getUsers(): Observable<User[]>{
-  return this.http.get<User[]>(this.baseUrl + 'users');
-}
+  getUser(id: number): Observable<User> {
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
+  }
 
-getUser(id: number): Observable<User>{
-  return this.http.get<User>(this.baseUrl + 'users/' + id);
-}
+  // tslint:disable-next-line: typedef
+  updateUser(id: number, user: User) {
+    return this.http.put(this.baseUrl + 'users/' + id, user);
+  }
 
-// tslint:disable-next-line: typedef
-updateUser(id: number, user: User){
-  return this.http.put(this.baseUrl + 'users/' + id, user);
-}
+  // tslint:disable-next-line: typedef
+  setMainPhoto(userId: number, id: number){
+    return this.http.post(this.baseUrl + 'users/' + userId + '/photos' + id + '/setMain', {});
+  }
 }
--- Projects/DatingApp/DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ Projects/DatingApp/DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -3,6 +3,8 @@ import { Photo } from 'src/app/_models/photo';
 import { FileUploader } from 'ng2-file-upload';
 import { environment } from 'src/environments/environment';
 import { AuthService } from 'src/app/_services/auth.service';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -15,7 +17,7 @@ export class PhotoEditorComponent implements OnInit {
   hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
  
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private userService: UserService, private alertify: AlertifyService) { }
 
   // tslint:disable-next-line: typedef
   ngOnInit() {
@@ -53,5 +55,13 @@ export class PhotoEditorComponent implements OnInit {
         this.photos.push(photo);
       }
     };
+  }
+
+  setMainPhoto(photo: Photo){
+    this.userService.setMainPhoto(this.authService.decodedToken.nameid, photo.id).subscribe(() => {
+
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 }
