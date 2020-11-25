35533b26e7f1f1366d37df14e33bf8c364998198
sarakhandaker <kttsara@gmail.com>
2020-11-24 16:03:27 -0800

    changing main photo on photo edit comp

--- Projects/DatingApp/DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ Projects/DatingApp/DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -16,6 +16,7 @@ export class PhotoEditorComponent implements OnInit {
   uploader: FileUploader;
   hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
+  currentMain: Photo;
  
   constructor(private authService: AuthService, private userService: UserService, private alertify: AlertifyService) { }
 
@@ -59,7 +60,9 @@ export class PhotoEditorComponent implements OnInit {
 
   setMainPhoto(photo: Photo){
     this.userService.setMainPhoto(this.authService.decodedToken.nameid, photo.id).subscribe(() => {
-      console.log("succes");
+      this.currentMain = this.photos.filter(p => p.isMain)[0];
+      this.currentMain.isMain = false;
+      photo.isMain = true;
     }, error => {
       this.alertify.error(error);
     });
