2c6c224a802bfa8c44b2468b811f6787f59ac9f0
sarakhandaker <kttsara@gmail.com>
2021-01-11 16:16:10 -0800

    using form service to build form

--- Projects/DatingApp/DatingApp-SPA/src/app/register/register.component.ts
+++ Projects/DatingApp/DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { EventEmitter, Input, Output } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AlertifyService } from '../_services/alertify.service';
 import { AuthService } from '../_services/auth.service';
 
@@ -14,17 +14,21 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   registerForm: FormGroup;
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService, private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.registerForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8) ]),
-      confirmPassword: new FormControl('', Validators.required)
-    }, this.matchPasswordValidator)
+    this.createRegisterForm();
   }
 
-  matchPasswordValidator(g: FormGroup){
+  createRegisterForm(){
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8) ]],
+      confirmPassword: ['', Validators.required]
+    }, {validator : this.passwordMatchValidator})
+  }
+
+  passwordMatchValidator(g: FormGroup){
     return g.get('password').value === g.get('confirmPassword').value ? null : {"mismatch": true};
   }
 
