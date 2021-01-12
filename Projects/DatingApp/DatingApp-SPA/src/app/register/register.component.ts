4e17b49c3efd8f1e8ab856db147cc6afcd395c7a
sarakhandaker <kttsara@gmail.com>
2021-01-11 16:23:59 -0800

    expanded register form

--- Projects/DatingApp/DatingApp-SPA/src/app/register/register.component.ts
+++ Projects/DatingApp/DatingApp-SPA/src/app/register/register.component.ts
@@ -21,11 +21,26 @@ export class RegisterComponent implements OnInit {
   }
 
   createRegisterForm(){
-    this.registerForm = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8) ]],
-      confirmPassword: ['', Validators.required]
-    }, {validator : this.passwordMatchValidator})
+    this.registerForm = this.fb.group(
+      {
+        gender: ['male'],
+        username: ['', Validators.required],
+        knownAs: ['', Validators.required],
+        dateOfBirth: [null, Validators.required],
+        city: ['', Validators.required],
+        country: ['', Validators.required],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(8)
+          ]
+        ],
+        confirmPassword: ['', Validators.required]
+      },
+      { validator: this.passwordMatchValidator }
+    );
   }
 
   passwordMatchValidator(g: FormGroup){
